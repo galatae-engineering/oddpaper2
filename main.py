312668661749342a995d6acaaed2b79723e6c1c9
rp <rp>
2026-03-19 14:51:00 +0100

    a

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def go_to_pose_at_specific_height(pose,height,r):
 
 def get_pose_from_xy(xy_coord,z):
   roll=math.atan(float(xy_coord[1])/xy_coord[0])*180/math.pi
-  return xy_coord+[z,180,90+roll]
+  return xy_coord+[z,180,90+20+roll]
 
 def place_sheet(r,default_speed,xy_coord,h_above_sheets,pump_pin,probe_speed):
   r.set_joint_speed(default_speed)
@@ -28,7 +28,7 @@ def pick_sheet(r,default_speed,xy_coord,h_above_sheets,pump_pin,probe_speed):
   GPIO.output(pump_pin,GPIO.HIGH)
   r.set_joint_speed(probe_speed)
   r.linear_probe(get_pose_from_xy(xy_coord,-150))
-  time.sleep(0.5)
+  time.sleep(1)
   r.linear_move_to_pose(get_pose_from_xy(xy_coord,h_above_sheets))
 
 def pick_and_place_sheet(pick_coord,place_coord,r,default_speed,h_above_sheets,pump_pin,probe_speed):
@@ -46,7 +46,7 @@ def main():
   delta_x=220+15
   delta_y=150+10
   number_of_piles=3
-  sheets_per_pile=1
+  sheets_per_pile=2
 
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(pump_pin,GPIO.OUT)
@@ -56,15 +56,11 @@ def main():
   r.set_joint_speed(default_speed)
 
   try:
-    for k in range(1):
-      """
-      for i in range(sheets_per_pile):
-        for j in range(number_of_piles):
-          pick_and_place_sheet([x_start,0],[x_start+delta_x,((1-number_of_piles)/2+j)*delta_y],r,default_speed,h_above_sheets,pump_pin,probe_speed)
-      """
-      for i in range(sheets_per_pile):
-        for j in range(number_of_piles):
-          pick_and_place_sheet([x_start+delta_x,((1-number_of_piles)/2+j)*delta_y],[x_start,0],r,default_speed,h_above_sheets,pump_pin,probe_speed)
+    for i in range(sheets_per_pile):
+      t=time.time()
+      for j in range(number_of_piles):
+        pick_and_place_sheet([x_start+delta_x,((1-number_of_piles)/2+j)*delta_y],[x_start,0],r,default_speed,h_above_sheets,pump_pin,probe_speed)
+      print("t:",time.time()-t)
 
   except:
     print("error:")
