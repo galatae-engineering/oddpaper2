7aa7c02c1809cad17fa024e99e2fbc0cc8a6d8fc
rp <rp>
2026-03-31 17:29:30 +0200

    kind of works with vac cleaner now

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import traceback
 
 def get_pose_from_point(point):
   roll=math.atan(float(point[1])/point[0])*180/math.pi
-  return point+[180,90+2+roll]
+  return point+[180,roll]
 
 def probe_at_defined_speed(xy_coord,speed,r):
   r.set_joint_speed(speed)
@@ -17,25 +17,21 @@ def probe_at_defined_speed(xy_coord,speed,r):
 
 def go_above_sheet(xy_coord,speed,r):
   r.set_joint_speed(speed)
-  r.go_to_pose(get_pose_from_point(xy_coord+[-60]))
+  r.go_to_pose(get_pose_from_point(xy_coord+[-20]))
 
 def place_sheet(r,default_speed,xy_coord,pump_pin):
-  go_above_sheet(xy_coord,default_speed,r)
   GPIO.output(pump_pin,GPIO.LOW)
-  probe_at_defined_speed(xy_coord,default_speed,r)
-  time.sleep(0.5)
   go_above_sheet(xy_coord,default_speed,r)
-  
+  probe_at_defined_speed(xy_coord,default_speed,r)
+  time.sleep(3)
+  go_above_sheet(xy_coord,default_speed,r)
 
 def pick_sheet(r,default_speed,xy_coord,pump_pin):
   go_above_sheet(xy_coord,default_speed,r)
   GPIO.output(pump_pin,GPIO.HIGH)
-  probe_at_defined_speed(xy_coord,5,r)
-  time.sleep(0.5)
-  #r.jog([0,0,50,0,0])
-  
+  probe_at_defined_speed(xy_coord,10,r)
   go_above_sheet(xy_coord,default_speed,r)
-  time.sleep(0.5)
+  #time.sleep(0.5)
 
 def pick_and_place_sheet(pick_coord,place_coord,r,default_speed,pump_pin):
   pick_sheet(r,default_speed,pick_coord,pump_pin)
@@ -43,13 +39,14 @@ def pick_and_place_sheet(pick_coord,place_coord,r,default_speed,pump_pin):
 
 def main():
   r=Robot(False)
-  pump_pin=4
+  pump_pin=5
   default_speed=50
   
   x_start=200
   delta_x=220+15
   delta_y=150+10
-  number_of_piles=3
+  number_of_piles=5
+  piles_per_line=5
 
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(pump_pin,GPIO.OUT)
@@ -58,10 +55,16 @@ def main():
   r.reset_and_home_joints()
 
   try:
-    for i in range(2):
+    for i in range(10):
       t=time.time()
-      for j in range(3):
-        pick_and_place_sheet([x_start+delta_x,((1-number_of_piles)/2+j)*delta_y],[x_start,0],r,default_speed,pump_pin)
+
+      first_line_N=min(number_of_piles,3)
+      for j in range(first_line_N):
+        pick_and_place_sheet([x_start+delta_x,((1-first_line_N)/2+j)*delta_y],[x_start,0],r,default_speed,pump_pin)
+      if(3<number_of_piles):
+        for j in range(number_of_piles-3):
+          pick_and_place_sheet([x_start,(2*j-1)*delta_y],[x_start,0],r,default_speed,pump_pin)
+          pass
       print("t:",time.time()-t)
 
   except:
